8369343a6145ead2a43be59f312a6f5efed3cfaa
Herbie <u1331078@utah.edu>
2023-08-13 10:40:13 -0600

    handle import errors

--- pointconvformer_wrapper/pcf_api.py
+++ pointconvformer_wrapper/pcf_api.py
@@ -1,8 +1,20 @@
 import os, sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '../pointconvformer'))
 
+try:
+    from pointconvformer.model_architecture import PCF_Backbone
+    from pointconvformer.model_architecture import get_default_configs
 
-from pointconvformer.model_architecture import PCF_Backbone
-from pointconvformer.model_architecture import get_default_configs
+except ImportError as e:
+    from logging import warning
 
+    warning('Failed to import PCF. Cannot use Encoder.')
+  
+    # if we can't import, mock them
+    class PCF_Backbone:
+        def __init__(self, *args, **kwargs) -> None:
+            raise NotImplementedError()
+        
+    def get_default_configs(*args, **kwargs):
+        raise NotImplementedError
 
